32952b444aa264a74c363f272ea58bfbdc6fed7a
GiacomoSorbi <g.sorbi@gmail.com>
2016-05-03 02:45:11 +0100

    countdown improved with real time updates

--- js/script.js
+++ js/script.js
@@ -1,16 +1,32 @@
 //ReactDOM.render(<h1><>, document.getElementById('app'));
 var Home = React.createClass({
-  render: function () {
+  getInitialState: function(){
+    //adds the nextDate property to state
+    return {nextDate: new Date(2016,4,7) - Date.now()};
+  },
+  componentDidMount: function(){
+  	//acts when the component is loaded
+    this.timer = setInterval(this.tick, 1000);
+  },
+  componentWillUnmount: function(){
+  	//acts then the component is removed
+    clearInterval(this.timer);
+  },
+  tick: function(){
+  	//function called by timer to update the nextDate variable
+  	this.setState({nextDate: new Date(2016,4,7) - Date.now()});
+  },
+  render: function(){
     return <div>
-    	<h1>Hello World, Hello Coders!</h1>
-    	<h2>Next Katathon is going to be in {Math.floor(this.props.nextDate/86400000)} days, 
-    	{Math.floor(this.props.nextDate%86400000/3600000)} hours, 
-    	{Math.floor(this.props.nextDate%3600000/60000)} minutes, 
-    	{Math.floor(this.props.nextDate%86400000/3600000)} hours, 
-    	{Math.floor(this.props.nextDate%60000/1000)} seconds</h2>
+      	<h1>Hello World, Hello Coders!</h1>
+    	<h2>Next Katathon is going to be in {Math.floor(this.state.nextDate/86400000)} days, 
+    	{Math.floor(this.state.nextDate%86400000/3600000)} hours, 
+    	{Math.floor(this.state.nextDate%3600000/60000)} minutes, 
+    	{Math.floor(this.state.nextDate%86400000/3600000)} hours, 
+    	{Math.floor(this.state.nextDate%60000/1000)} seconds</h2>
     	<p>[Not true, just making tests...]</p>
     	</div>;
   }
 });
-var nextKatathon=(new Date(2016,4,7) - Date.now());
-ReactDOM.render(<Home nextDate={nextKatathon} />, document.getElementById('app'));
+// var nextKatathon=(new Date(2016,4,7) - Date.now());
+ReactDOM.render(<Home/>, document.getElementById('app'));
