1d7ff809f8d7f9b15b58f4a36bdbf1fb8df80405
GiacomoSorbi <g.sorbi@gmail.com>
2016-05-03 02:16:47 +0100

    rudimental countdown added

--- js/script.js
+++ js/script.js
@@ -3,10 +3,14 @@ var Home = React.createClass({
   render: function () {
     return <div>
     	<h1>Hello World, Hello Coders!</h1>
-    	<h2>Next Katathon is going to be on {this.props.date}</h2>
+    	<h2>Next Katathon is going to be in {Math.floor(this.props.nextDate/86400000)} days, 
+    	{Math.floor(this.props.nextDate%86400000/3600000)} hours, 
+    	{Math.floor(this.props.nextDate%3600000/60000)} minutes, 
+    	{Math.floor(this.props.nextDate%86400000/3600000)} hours, 
+    	{Math.floor(this.props.nextDate%60000/1000)} seconds</h2>
     	<p>[Not true, just making tests...]</p>
     	</div>;
   }
 });
-var nextKatathon=(new Date(2016,5,7)).toString();
-ReactDOM.render(<Home date={nextKatathon} />, document.getElementById('app'));
+var nextKatathon=(new Date(2016,4,7) - Date.now());
+ReactDOM.render(<Home nextDate={nextKatathon} />, document.getElementById('app'));
