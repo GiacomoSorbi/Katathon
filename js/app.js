c078ec18d0c7b6f9a3370263f947ae87f6da40bd
GiacomoSorbi <g.sorbi@gmail.com>
2016-05-15 23:09:36 +0100

    added header separated component

--- js/app.js
+++ js/app.js
@@ -2,38 +2,39 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import Footer from "./layout/Footer";
+import Header from "./layout/Header";
 // import pippi from "./layout/footer";
 // if (pippi) console.log(pippi);
 // else console.log("no pippi");
 
-var Header = React.createClass({
-  render: function () {
-    return (
-      <nav className="navbar navbar-default">
-        <div className="container-fluid">
-          <div className="navbar-header">
-            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-              <span className="sr-only">Toggle navigation</span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-            </button>
-            <a className="navbar-brand" href="#">Katathon</a>
-          </div>
-          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul className="nav navbar-nav">
-              <li className="active"><a href="#">Link <span className="sr-only">Home</span></a></li>
-              <li><a href="#">About</a></li>
-              <li><a href="#">Resources</a></li>
-              <li><a href="#">Join Us</a></li>
-              <li><a href="#">Follow Us</a></li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    );
-  }
-});
+// var Header = React.createClass({
+//   render: function () {
+//     return (
+//       <nav className="navbar navbar-default">
+//         <div className="container-fluid">
+//           <div className="navbar-header">
+//             <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+//               <span className="sr-only">Toggle navigation</span>
+//               <span className="icon-bar"></span>
+//               <span className="icon-bar"></span>
+//               <span className="icon-bar"></span>
+//             </button>
+//             <a className="navbar-brand" href="#">Katathon</a>
+//           </div>
+//           <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+//             <ul className="nav navbar-nav">
+//               <li className="active"><a href="#">Link <span className="sr-only">Home</span></a></li>
+//               <li><a href="#">About</a></li>
+//               <li><a href="#">Resources</a></li>
+//               <li><a href="#">Join Us</a></li>
+//               <li><a href="#">Follow Us</a></li>
+//             </ul>
+//           </div>
+//         </div>
+//       </nav>
+//     );
+//   }
+// });
 
 // var Footer = React.createClass({
 //   render: function () {
