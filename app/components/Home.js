1ce17d8626d7b9c9904119f4953a896a8cd7d372
Alex Brown <alexanderbrown.info@gmail.com>
2017-01-16 20:28:14 +0000

    tweaked font settings in timer component

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,16 +1,10 @@
 var React = require("react");
-var Header = require("./Header");
 var HomeBanner = require("./Home-banner");
 
 var Home = React.createClass({
 
   render: function(){
-    return (
-    	<div>
-      <Header/>
-      <HomeBanner/>
-    	</div>
-    );
+    return <HomeBanner/>;
   }
 });
 
