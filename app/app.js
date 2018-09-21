40ce6c188001384132873760fb8234bbf47e965c
annaVih <anna-kobzii@mail.ru>
2018-09-21 14:56:54 +0100

    Move store file to the redux folder

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, IndexRoute, HashRouter, browserHistory } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './store';
+import store from './redux/store';
 
 //Components
 import Page from './components/Page';
--- app/redux/store.js
+++ app/redux/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import rootReducer from './redux/reducers';
+import rootReducer from './reducers';
 const initialState = {};
 const middleware = [thunk];
 
