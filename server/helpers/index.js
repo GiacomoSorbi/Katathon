ac09af24418322e61a868666d46af8237cf3b83a
annaVih <anna-kobzii@mail.ru>
2018-10-01 01:46:47 +0100

    Create a helper method to convert date to proper date string

--- server/helpers/index.js
+++ server/helpers/index.js
@@ -1,7 +1,8 @@
-module.exports = {
-  getRecentEvent(events) {
-    const newEvents = events.filter(event => event.date >= (Date.now() - 43200))
-    const nextDate = newEvents.reduce((acc, next) => acc.date < next.date ? acc : next)
-    return events.find(event => event.date === nextDate.date)
-  }
+
+export const getRecentEvent = events => {
+  const newEvents = events.filter(event => event.date >= (Date.now() - 43200))
+  const nextDate = newEvents.reduce((acc, next) => acc.date < next.date ? acc : next)
+  return events.find(event => event.date === nextDate.date)
 }
+
+export const dateToString = date => new Date(date).toDateString()
