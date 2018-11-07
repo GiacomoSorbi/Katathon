e041b9ce3017ded272042a7d8aa35f35b001f6ee
Chorley <jake.chorley@M10361.local>
2018-11-07 12:41:49 +0000

    Add katathon updateAllScores job

---
+++ server/jobs/index.js
@@ -0,0 +1,20 @@
+import { scheduleJob } from 'node-schedule'
+
+import { updateAllScores } from '../helpers/index'
+
+// Ensures job only runs during Katathon
+export const newKatathonJob = (timestamp, katathonId) => {
+  const start = new Date(timestamp)
+  const end = new Date(timestamp)
+  end.setHours(end.getHours() + 24)
+  scheduleJob(
+    {
+      start,
+      end,
+      rule: '*/10 * * * *'
+    },
+    () => {
+      updateAllScores(katathonId)
+    }
+  )
+}
